abec04c63483a532be73bb042952b002c787c4af
dimiandre <dimi@stake.fish>
2021-02-12 23:33:38 +0100

    add logs

--- scripts/pin.js
+++ scripts/pin.js
@@ -1,10 +1,14 @@
 console.log("Pinning website...");
 
 const ENV = process.env;
+
+console.log("ENV");
 if (ENV.npm_config_github_action) {
   console.log(
     "secret ",
     ENV.npm_config_pinata_api_key,
     ENV.npm_config_pinata_secret_key
   );
+} else {
+  console.log("env ", ENV.PINATA_API_KEY, ENV.PINATA_SECRET_API_KEY);
 }
