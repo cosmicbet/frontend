41dba7550687a2dae2ce7907ae1e206033a5b3f8
dimiandre <dimi@stake.fish>
2021-02-13 00:09:53 +0100

    add pinning script and demo url

--- scripts/pin.js
+++ scripts/pin.js
@@ -1,14 +1,50 @@
 console.log("Pinning website...");
-
+const axios = require("axios");
+const fs = require("fs");
+const FormData = require("form-data");
+const recursive = require("recursive-fs");
 const ENV = process.env;
 
-console.log("ENV");
-if (ENV.npm_config_github_action) {
-  console.log(
-    "secret ",
-    ENV.npm_config_pinata_api_key,
-    ENV.npm_config_pinata_secret_key
-  );
-} else {
-  console.log("env ", ENV.PINATA_API_KEY, ENV.PINATA_SECRET_API_KEY);
-}
+const pinataApiKey = ENV.PINATA_API_KEY ? PINATA_API_KEY : "";
+const pinataSecretApiKey = ENV.PINATA_SECRET_API_KEY
+  ? PINATA_SECRET_API_KEY
+  : "";
+
+const folder = "public";
+const name = "cosmic.bet";
+
+const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+
+recursive.readdirr(folder, function (err, dirs, files) {
+  let data = new FormData();
+  files.forEach((file) => {
+    data.append(`file`, fs.createReadStream(file), {
+      filepath: file,
+    });
+  });
+
+  const metadata = JSON.stringify({
+    name,
+  });
+  data.append("pinataMetadata", metadata);
+
+  return axios
+    .post(url, data, {
+      maxContentLength: "Infinity",
+      headers: {
+        "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+        pinata_api_key: pinataApiKey,
+        pinata_secret_api_key: pinataSecretApiKey,
+      },
+    })
+    .then(function (response) {
+      const pin = response.data.IpfsHash;
+      console.log("Pinned Hash: ", pin);
+
+      const gatewayUrl = "https://gateway.pinata.cloud/ipfs/" + pin;
+      console.log("Demo URL: " + gatewayUrl);
+    })
+    .catch(function (error) {
+      console.log("Error when pinning using Pinata", error);
+    });
+});
