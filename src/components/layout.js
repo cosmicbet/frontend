41ea542c8eec1bab17ba755e185b85eb54cf5c40
dimiandre <dimi@stake.fish>
2021-02-16 15:22:08 +0100

    [ui] add disclaimer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
+import { Link } from "gatsby";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -17,6 +18,11 @@ const Footer = styled.div`
   margin-top: 200px;
 `;
 
+const Disclaimer = styled.p`
+  font-size: 0.8rem;
+  color: grey;
+`;
+
 export default function Layout({ children }) {
   return (
     <Container>
@@ -25,7 +31,12 @@ export default function Layout({ children }) {
       <Footer>
         <hr />
         Cosmic Casino | <a href="">GitHub</a> | <a href="">Telegram</a> |{" "}
-        <a href="">Discord</a> |
+        <a href="">Discord</a>
+        <Disclaimer>
+          Disclaimer. All betting in any forms involves risk. Be aware and
+          accept this risk before betting. Never bet with money you cannot
+          afford to lose. <Link to="/disclaimer">Read More</Link>{" "}
+        </Disclaimer>
       </Footer>
     </Container>
   );
